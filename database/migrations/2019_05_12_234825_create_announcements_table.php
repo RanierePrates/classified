65adcef882a0d71d35c5d675d1321003ff7efb97
Raniere Prates <raniere.prates@gmail.com>
2019-05-12 21:41:05 -0300

    criei models e migrations dos anuncios e categorias dos anuncios

---
+++ database/migrations/2019_05_12_234825_create_announcements_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAnnouncementsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('announcements', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->string('title');
+            $table->string('photo');            
+            $table->text('description');          
+            $table->float('price');          
+            $table->integer('categorie_id');          
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('announcements');
+    }
+}
