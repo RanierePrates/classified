ff20f50b013379272160060f1fb45c838091c8f7
rsilva <rsilva@uplexis.com.br>
2019-05-13 17:54:20 -0300

    criei controller e rotas dos anúncios

--- app/Announcement.php
+++ app/Announcement.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Announcement extends Model
 {
-    //
+    protected $fillable = ['user_id', 'title', 'photo', 'description', 'price', 'categorie_id'];
 }
